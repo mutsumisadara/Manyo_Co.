c22b8aa3a36c684d3eeffb408dc80866148e4af2
mutsumisadara <mutsumisadara@gmail.com>
2023-01-09 13:13:19 +0900

    add rspec

--- db/migrate/20230106072558_create_tasks.rb
+++ db/migrate/20230106072558_create_tasks.rb
@@ -1,8 +1,12 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
+  # change_column :tasks, :title, :string, null: false
+  # change_column :tasks, :title, :string, unique: true
+  # change_column :tasks, :to_do, :text, null: false
+
   def change
     create_table :tasks do |t|
-      t.string :title
-      t.text :to_do
+      t.string :title, null: false, unique: true
+      t.text :to_do, null: false, unique: true
 
       t.timestamps
     end
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'タスクモデル機能', type: :model do
+  describe 'バリデーションのテスト' do
+    context 'タスクのタイトルが空の場合' do
+      it 'バリデーションにひっかる' do
+        task = Task.new(title: '', to_do: '失敗テスト')
+        expect(task).not_to be_valid
+      end
+    end
+    context 'タスクの詳細が空の場合' do
+      it 'バリデーションにひっかかる' do
+        task = Task.new(title: '失敗テスト', to_do: '')
+        expect(task).not_to be_valid
+        # ここに内容を記載する
+      end
+    end
+    context 'タスクのタイトルと詳細に内容が記載されている場合' do
+      it 'バリデーションが通る' do
+        task = Task.new(title: '成功', to_do: 'テスト')
+        expect(task).to be_valid
+        # ここに内容を記載する
+      end
+    end
+  end
+  pending "add some examples to (or delete) #{__FILE__}"
+end
