24baa843c1bf8473b31baa247c8147fb08deca50
mutsumisadara <mutsumisadara@gmail.com>
2023-01-10 15:27:47 +0900

    add language

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -19,10 +19,16 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
   end
+  descrive '降順機能' do
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        # task = FactoryBot.create(:task, title: 'タイトル', to_do: '内容')
+        # task_list = all('.task_row') 
+        # visit tasks_path
+      end
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-
        end
      end
   end
