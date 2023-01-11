c4b7d429e37854ccc15fba7fbc7733dcfd9a1975
mutsumisadara <mutsumisadara@gmail.com>
2023-01-11 09:27:03 +0900

    update rspec

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -6,29 +6,45 @@ RSpec.describe 'タスク管理機能', type: :system do
   end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
-      it '作成したタスクが表示される' do
+      example '作成したタスクが表示される' do
+        visit new_task_path
+        fill_in 'task[title]', with: 'task'
+        fill_in 'task_to_do', with: '詳細'
+        click_button '登録する'
+        expect(page).to have_content 'task'
+        expect(page).to have_content '詳細'
       end
     end
   end
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
-      it '作成済みのタスク一覧が表示される' do
+      example '作成済みのタスク一覧が表示される' do
         task = FactoryBot.create(:task, title: 'タイトル', to_do: '内容')
-        visit tasks_path
-        # expect(page).to have_content 'task'
+        visit root_path
+        expect(page).to have_content 'タイトル'
+        expect(page).to have_content '内容'
+      end
+    end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      example '新しいタスクが一番上に表示される' do
+        task = FactoryBot.create(:task, title: 'タイトル1', to_do: '内容1')
+        second_task = FactoryBot.create(:second_task, title: 'タイトル2', to_do: '内容2')
+        # visit tasks_path = visit "/tasks"
+        visit "/tasks"
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'タイトル2'
+        expect(task_list[1]).to have_content 'タイトル1'
       end
     end
   end
-  descrive '降順機能' do
-    context 'タスクが作成日時の降順に並んでいる場合' do
-      it '新しいタスクが一番上に表示される' do
-        # task = FactoryBot.create(:task, title: 'タイトル', to_do: '内容')
-        # task_list = all('.task_row') 
-        # visit tasks_path
-      end
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
-       it '該当タスクの内容が表示される' do
+       example '該当タスクの内容が表示される' do
+        task = FactoryBot.create(:task, title: 'タイトル', to_do: '内容')
+        visit tasks_path(Task.last.id) #詳細画面タスクのid取得、多分
+        click_link '詳細', match: :first #複数の要素の中から最初の要素を指定
+        expect(page).to have_content 'タイトル'
+        expect(page).to have_content '詳細'
        end
      end
   end
