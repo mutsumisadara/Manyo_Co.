5b214364689e0be0f82f8f15dde76c24ada51b9a
mutsumisadara <mutsumisadara@gmail.com>
2023-01-24 20:03:49 +0900

    fix a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 2.times do |n|
     User.create!(
         name: "sadara#{n + 1}",
-        email: "sadaraaaaa#{n + 1}@gmail.com",
+        email: "sadaramutsumi#{n + 1}@gmail.com",
         password: "sadaramutsumi#{n + 1}",
         password_confirmation: "sadaramutsumi#{n + 1}"
     )
