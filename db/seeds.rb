0904cb495ead6b453899b1e5b386234fafabef92
mutsumisadara <mutsumisadara@gmail.com>
2023-01-24 18:22:49 +0900

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
         password_confirmation: "sadaramutsumi#{n + 1}"
     )
 end
+
+User.create!(
+    name: 'admin'
+    email: admin@gmail.com
+    password: adminuser
+    password_confirmation: adminuser
+    admin: true
+    )
