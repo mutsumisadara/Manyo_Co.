33acce70fbc819463f050fe1ae2bac5db643910e
mutsumisadara <mutsumisadara@gmail.com>
2023-01-24 11:52:10 +0900

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # root to: 'tasks#index'
+  root to: 'tasks#index'
   resources :sessions, only: [:new, :create, :show, :destroy]
   resources :users, only: [:new, :create, :show] # 追加でrails routes確認
   resources :tasks
